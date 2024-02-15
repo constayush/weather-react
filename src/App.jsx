18bb388b98c66bafe42693f928a98dd2511fc47f
constayush <aayush@mail.com>
2024-02-15 20:52:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className=' wrapper w-screen h-screen flex justify-center items-center gap-[2rem]'>
+
+
+
+
+
+      <div className=' w-[55vw] h-[80vh] curv flex flex-col justify-center items-center gap-[2rem] ' >
+
+        <div className='subSec glass w-[100%] h-[50%] curv'></div>
+        <div className='subSec glass w-[100%] h-[30%] curv'></div>
+
+        <div className='subSec  w-[100%] h-[20%] flex justify-center itmes-center gap-[2rem] '>
+          <div className='subSec glass w-[49%] h-[100%] curv'></div>
+          <div className='subSec glass w-[49%] h-[100%] curv'></div>
+        </div>
+
+
+
+      </div>
+
+
+
+
+
+
+
+      <div className=' w-[30vw] h-[80vh] curv  flex flex-col gap-[2rem]' >
+
+
+<div className='searchbar w-full h-[10vh] glass  curv'> </div>
+
+
+<div className='glass flex flex-col  curv'>
+<div className='w-full h-[23vh]  curv'></div>
+<hr/>
+<div className='w-full h-[23vh]  curv'></div>
+<hr/>
+<div className='w-full h-[23vh]  curv'></div>
+</div>
+
+
+      </div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App

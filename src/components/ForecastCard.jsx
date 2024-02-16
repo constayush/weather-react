ad6a9cadb7f0bfa5fdab74e6e012499e0eac4089
constayush <aayush@mail.com>
2024-02-16 23:39:09 +0530

    second section ui created

--- src/components/ForecastCard.jsx
+++ src/components/ForecastCard.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
-
+import searchIcon from '../../public/images/searchIcon.svg'
 function ForecastCard() {
   return (
-<div className='mainWeatherCard w-[20rem] lg:w-[30rem] h-[32.5rem] glass rounded-lg'></div>
+<div className='mainWeatherCard w-[20rem] lg:w-[30rem] h-[32.5rem] glass rounded-lg p-4'>
+
+
+
+
+
+
+
+<div className='flex flex-col gap-[1rem] w-full h-full p-0'>
+
+
+<div className='searchbar w-full glassBlack h-[10%] rounded-lg flex '>
+
+ <input className='w-full rounded-lg searchFeild' type='text'></input>
+  <img className='w-[2.5rem] m-[1rem]' src={searchIcon}/>
+
+ 
+</div>
+
+
+
+
+<div className='w-full h-[17%] glassdark rounded-lg flex justify-between p-4 text-[1.3rem]'><h1>friday</h1> <h1>0°c</h1></div>
+<div className='w-full h-[17%] glassdark rounded-lg flex justify-between p-4 text-[1.3rem]'><h1>friday</h1> <h1>0°c</h1></div>
+<div className='w-full h-[17%] glassdark rounded-lg flex justify-between p-4 text-[1.3rem]'><h1>friday</h1> <h1>0°c</h1></div>
+<div className='w-full h-[17%] glassdark rounded-lg flex justify-between p-4 text-[1.3rem]'><h1>friday</h1> <h1>0°c</h1></div>
+<div className='w-full h-[17%] glassdark rounded-lg flex justify-between p-4 text-[1.3rem]'><h1>friday</h1> <h1>0°c</h1></div>
+</div>
+
+
+
+</div>
   )
 }
 

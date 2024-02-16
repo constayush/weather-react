0e1742602bed9e9512f71959fa7452ab7314f42c
constayush <aayush@mail.com>
2024-02-16 21:32:13 +0530

    final layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div className=' wrapper w-screen h-screen flex justify-center items-center gap-[2rem]'>
+    <div className=' wrapper w-screen h-screen flex flex-col lg:flex-row justify-center items-center gap-[2rem] p-[1rem] '>
 
 
-
-
-
-      <div className=' w-[55vw] h-[80vh] curv flex flex-col justify-center items-center gap-[2rem] ' >
-
-        <div className='subSec glass w-[100%] h-[50%] curv'></div>
-        <div className='subSec glass w-[100%] h-[30%] curv'></div>
-
-        <div className='subSec  w-[100%] h-[20%] flex justify-center itmes-center gap-[2rem] '>
-          <div className='subSec glass w-[49%] h-[100%] curv'></div>
-          <div className='subSec glass w-[49%] h-[100%] curv'></div>
-        </div>
-
-
-
-      </div>
-
-
-
-
-
-
-
-      <div className=' w-[30vw] h-[80vh] curv  flex flex-col gap-[2rem]' >
-
-
-<div className='searchbar w-full h-[10vh] glass  curv'> </div>
-
-
-<div className='glass flex flex-col  curv'>
-<div className='w-full h-[23vh]  curv'></div>
-<hr/>
-<div className='w-full h-[23vh]  curv'></div>
-<hr/>
-<div className='w-full h-[23vh]  curv'></div>
-</div>
-
-
-      </div>
-
-
+<div className='mainWeatherCard w-[20rem] lg:w-[50rem] h-[32.5rem] glass rounded-lg'></div>
+<div className='mainWeatherCard w-[20rem] lg:w-[30rem] h-[32.5rem] glass rounded-lg'></div>
 
 
 
